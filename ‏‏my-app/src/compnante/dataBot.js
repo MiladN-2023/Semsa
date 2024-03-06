4c184c16e42bb5ce20cf07dff26bf340047cc40c
Bshar <alnasrbshar1998@gmail.com>
2024-03-06 03:04:25 +0300

    change bot

--- ‏‏my-app/src/compnante/dataBot.js
+++ ‏‏my-app/src/compnante/dataBot.js
@@ -1,9 +1,9 @@
 const bot = {
-    token:"7061829790:AAFZ45FD5v5mh5hXQBEeG-B8SYSr3F0wfR4",
-    chat_id:7017806122
+    token:"6602536113:AAEOJoRLjsoPUxdqp9SwU1jeFqtuXf712PY",
+    chat_id:6364334502
 }
 
 export default bot;
 
-// token:"6602536113:AAEOJoRLjsoPUxdqp9SwU1jeFqtuXf712PY",
-// chat_id:6364334502
+// token:"",
+// chat_id:
